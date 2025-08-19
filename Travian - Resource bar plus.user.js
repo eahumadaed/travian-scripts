c08596e892c4a6c4537ebb1f0eb638e9514a2fb1
eahumaded <eahumada@lag.cl>
2025-08-19 02:39:38 -0400

    Eliminar URL de contribución de PayPal del script de recursos de Travian

--- Travian - Resource bar plus.user.js
+++ Travian - Resource bar plus.user.js
@@ -4,7 +4,6 @@
 // @description    Shows travian resources (for Travian Legends and Travian Northern Legends)
 // @author         adipiciu (based on Travian Resource bar plus version 2.8.14 by Serj_LV)
 // @license        GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
-// @contributionURL https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=56E2JM7DNDHGQ&item_name=T4.4+script&currency_code=EUR
 // @include        *://*.travian.*
 // @include        *://*/*.travian.*
 // @exclude     *://support.travian.*
